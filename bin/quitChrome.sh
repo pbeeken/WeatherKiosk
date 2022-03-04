fdbb907277005f5c987033539ab096d703afeeb8
pbeeken <pbeeken@byramhills.org>
2022-03-04 15:49:44 -0500

    consolidated shell scripts

---
+++ bin/quitChrome.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# unclutter needs to be running
+export DISPLAY=:0
+#sleep 10
+xdotool key Alt+F4
+
---
+++ bin/shutDown.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# quit chrome responsibly
+/home/pi/WeatherKiosk/bin/quitChrome.sh
+# schedule shutdown
+sudo shutdown 1
+
