f595984f8f106ceab5b5da069f269189efd7b12a
pbeeken <pbeeken@byramhills.org>
2022-03-06 10:39:12 -0500

    add network status cgi-bin

---
+++ cgi-bin/networkStatus.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# set default address
+if [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  addr="$1"
+else
+  addr="8.8.8.8"
+fi
+echo "$addr"
+# test reachability
+status="`ping $addr -w 1 -c 1 | grep 'packet loss'`"
+([[ "$status" =~ \ 100\% ]]) && echo "&#11015; DN" || echo "&#11014; UP"
+
+
+
