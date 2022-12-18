67ad1ca26bcf6ac657baccc2253ce242adf9295c
P. Beeken <pbeeken@byramhills.org>
2022-12-17 21:14:03 -0500

    temporarily added back old scripts

---
+++ bin/testRotate.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#
+# This tests if the unclutter daemon is running and launches
+# it if it isn't running. rotateTabs needs unclutter to pass
+# keystrokes to the foremost application in the xwindow.
+#
+pid=0`pidof unclutter`
+if [ $pid -gt 0 ]; then
+  echo "unclutter operational"
+else
+  echo "launching unclutter..."
+  unclutter &
+fi
+
+pid=0$(pidof -x rotateTabs.sh)
+if [ $pid -gt 0 ]; then
+  echo "rotateTabs operational"
+else
+  echo "launching rotateTabs..."
+  /home/pi/WeatherKiosk/bin/rotateTabs.sh &
+fi
+
+echo 0
