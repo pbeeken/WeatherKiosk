d14cc7a92d275691ffcfd9815c990aab1cd8fe55
pbeeken <pbeeken@byramhills.org>
2026-03-11 09:20:41 -0400

    added the shell that runs from cron to collect the weather data via OCR of the display screens.

--- bin/collectWeatherData.sh
+++ bin/collectWeatherData.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
+# NOTE THAT THIS CANNOT RUN ON DEVELOPER ENVIRONMENT. ONLY ON THE RASBERRY PI.
 # Get wind or wave data based on the command line switch
 # Define the valid options (e.g., 'a' and 'b')
 # The leading colon ':' enables silent error reporting
 cd /home/pi/WeatherKiosk/bin
+
+SOURCE="exrx"
+WAVE_ENABLED=false
+WIND_ENABLED=false
 
 # Loop through all arguments provided to the script
 while [[ "$#" -gt 0 ]]; do
     case $1 in
         # gather wind data
         -z|--wind)
-            echo "Getting wind data from OCR"
-            python3 captureBuoyData.py -z -s exrx
+            echo "Getting wind data from ${@: -1} via OCR"
+            WIND_ENABLED=true
             shift # Move to the next argument
             ;;
         -w|--wave)
-            echo "Getting wave data from OCR"
-            python3 captureBuoyData.py -w -s exrx
+            echo "Getting wave data from ${@: -1} via OCR"
+            WAVE_ENABLED=true
+            shift # Move to the next argument
+            ;;
+	exrx|wlis|clis|all)
+            SOURCE=$1
             shift # Move to the next argument
             ;;
         *)
@@ -25,3 +34,15 @@ while [[ "$#" -gt 0 ]]; do
             ;;
     esac
 done
+
+if [ -n "$1" ]; then
+    SOURCE="$1"
+fi
+
+if [ $WAVE_ENABLED = true ]; then
+     /bin/python3 captureBuoyData.py -w -s $SOURCE
+fi
+
+if [ $WIND_ENABLED = true ]; then
+     /bin/python3 captureBuoyData.py -z -s $SOURCE
+fi
